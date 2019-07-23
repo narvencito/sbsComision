da2bb1ce1dd9ab06a6282fef60ac15eabdbbbd38
narvencito <narvencito@gmail.com>
2019-07-23 15:05:47 -0500

    modify port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
+app.set('port', process.env.PORT || 5000);
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({limit:'10mb'}));
@@ -10,7 +11,11 @@ app.use(bodyParser.json({limit:'10mb'}));
 app.use('/comision', require('./controllers/comisionController'));
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3005, () => console.log('Example app listening on port 3005!'));
+
+app.listen(app.get("port"), () =>{
+    console.log("server run in port "+ app.get('port'));
+    
+    })
 
 
 
