4bb29cfb275a3ba586550356ba3a9ecdfc317f45
narven <narvencito@gmail.com>
2020-12-18 17:54:27 -0500

    add solve captcha

--- src/controllers/afiliadoController.js
+++ src/controllers/afiliadoController.js
@@ -3,11 +3,24 @@ const express = require('express');
 const router = express.Router();
 //const url = 'https://www2.sbs.gob.pe/afiliados/paginas/consulta.aspx';
 const url = 'https://reportedeudas.sbs.gob.pe/afiliados/paginas/Consulta.aspx';
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const cheerio = require('cheerio');
 const aModel = require('../models/afiliado.model');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 // const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 // puppeteer.use(StealthPlugin());
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+puppeteer.use(
+ // StealthPlugin(),
+  RecaptchaPlugin({
+     provider: {
+       id: '2captcha',
+       token: '2d8bccd99cb847be44e3daefbf85a50f', // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+     },
+    visualFeedback: true, // colorize reCAPTCHAs (violet = detected, green = solved)
+  })
+);
 
 router.post('/spp', async (req, res) => {
   console.log("inicio consulta spp");
@@ -25,16 +38,20 @@ router.post('/spp', async (req, res) => {
          // '--ignore-certificate-errors'
         ]
       });
+      
       //const context = await browser.();
       const page = await browser.newPage();
       //await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
       await page.goto(url);
       await page.focus('#cphContent_txtDocumento');
-      await new Promise(r => setTimeout(r, 550));
+      //await new Promise(r => setTimeout(r, 550));
       await page.evaluate(val => document.querySelector('#cphContent_txtDocumento').value = val, pDni);
-      await new Promise(r => setTimeout(r, 550));
-      await page.click('input[type="submit"]', {waitUntil: 'domcontentloaded'});
-      await page.waitForNavigation({delay:350});
+      //await new Promise(r => setTimeout(r, 550));
+      await Promise.all([
+           page.solveRecaptchas(),
+           page.waitForNavigation(),
+           page.click('#cphContent_btnBuscar'),
+         ]);
       var html = await page.content();
       const $ = cheerio.load(html);
       $('tr > td > span').toArray().map(item => {
