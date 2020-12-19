757a2220459825983bb484ef16ff080cabf0e539
narven <narvencito@gmail.com>
2020-12-19 07:24:17 -0500

    deploy and change model spp

--- src/controllers/afiliadoController.js
+++ src/controllers/afiliadoController.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const url = 'https://reportedeudas.sbs.gob.pe/afiliados/paginas/Consulta.aspx';
 const puppeteer = require('puppeteer-extra');
 const cheerio = require('cheerio');
+const proxyChain = require('proxy-chain');
 const aModel = require('../models/afiliado.model');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
@@ -22,22 +23,48 @@ puppeteer.use(
   })
 );
 
+const proxyUrl = 'http://proxy.apify.com:8000';
+const username = 'groups-RESIDENTIAL';
+const password = 'iWycxsC3644Hk4qsmkeKt8g65';
+
+
 router.post('/spp', async (req, res) => {
   console.log("inicio consulta spp");
   var pDni = req.body.dni;
   var arr = [];
   var status = false;
   var msn = "";
+  const oldProxyUrl = 'http://auto:iWycxsC3644Hk4qsmkeKt8g65@proxy.apify.com:8000';
+  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+  console.log("pro ",newProxyUrl);
+  const browser = await puppeteer.launch({
+    args: [`--proxy-server=${newProxyUrl}`],
+    ignoreHTTPSErrors: true,
+    headless: false,
+    //headless: false,
+    'args': [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ]
+  });
   try {
-      const browser = await puppeteer.launch({
-        headless: false,
-        'args': [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ]
-      });
       
       const page = await browser.newPage();
+     // const page2 = await browser.newPage();
+      await page.authenticate({ username, password });
+    // //   await page.setExtraHTTPHeaders({
+    // //     'Proxy-Authorization': 'Basic ' + Buffer.from(':').toString('base64'),
+    // // });
+    //   await page2.goto("https://www.cual-es-mi-ip.net", { waitUntil: 'networkidle0' });
+    //   console.log("await");
+
+    //   await new Promise(r => setTimeout(r, 350));
+    //   console.log("continue");
+    //   await page2.screenshot({ path: 'example.png' });
+    //   var html = await page2.content();
+    //   const $ = cheerio.load(html);
+    //   console.log("==== ", $);
+   
       await page.goto(url);
       await page.focus('#cphContent_txtDocumento');
       await page.evaluate(val => document.querySelector('#cphContent_txtDocumento').value = val, pDni);
@@ -60,23 +87,23 @@ router.post('/spp', async (req, res) => {
         }else{
           status = true;
           msn = "registro encontrado";
-          model.dni = arr[1];
-          model.apellidoPaterno = arr[2];
-          model.apellidoMaterno = arr[4];
-          model.primerNombre = arr[6];
+          model.documento = arr[1];
+          model.apellido_paterno = arr[2];
+          model.apellido_materno = arr[4];
+          model.primer_nombre = arr[6];
           model.nacionalidad = arr[7];
-          model.segundoNombre = arr[8];
+          model.segundo_nombre = arr[8];
           model.sexo = arr[12];
-          model.lugarResidencia = arr[9] +"-"+arr[10]+"-"+arr[11];
-          model.estadoCivil = arr[13];
-          model.codigoAfiliado = arr[14];
-          model.origenAfiliado = arr[15];
-          model.situacionAfiliado = arr[16];
-          model.tipoTrabajador = arr[17];
-          model.tipoComision = arr[18];
-          model.afpActual = arr[19];
-          model.fechaDefuncion = arr[20];
-          model.fechaIngresoSpp = arr[21];
+          model.lugar_residencia = arr[9] +"-"+arr[10]+"-"+arr[11];
+          model.estado_civil = arr[13];
+          model.cuss = arr[14];
+          model.origen_afiliado = arr[15];
+          model.situacion = arr[16];
+          model.tipo_trabajador = arr[17];
+          model.tipo_comision = arr[18];
+          model.afp_actual = arr[19];
+          model.fecha_defuncion = arr[20];
+          model.fecha_afiliacion = arr[21];
         }
         
       }else{
@@ -90,16 +117,21 @@ router.post('/spp', async (req, res) => {
         data: model,
         message: msn
       });
-      //await context.close();
+      await context.close();
+      //res.status(200).send({ok:true});
       await browser.close();
+      await proxyChain.closeAnonymizedProxy(newProxyUrl, true);
 
     } catch (error) {
     console.log("try error " + error);
+    
     res.status(200).send({
       ok: false,
       data: null,
       message: "error"
     });
+   await browser.close();
+   await proxyChain.closeAnonymizedProxy(newProxyUrl, true);
   }
 
 });
--- src/models/afiliado.model.js
+++ src/models/afiliado.model.js
@@ -1,24 +1,25 @@
 class AfiliadoModel  {
   
-    constructor(dni, apellidoPaterno, apellidoMaterno, primerNombre, segundoNombre, sexo, nacionalidad, lugarResidencia, estadoCivil, codigoAfiliado, origenAfiliado, situacionAfiliado, tipoTrabajador, afpActual, tipoComision, fechaDefuncion, fechaIngresoSpp){
-          this.dni = dni;
-          this.apellidoPaterno = apellidoPaterno;
-          this.apellidoMaterno = apellidoMaterno;
-          this.primerNombre = primerNombre;
-          this.segundoNombre = segundoNombre;
-          this.sexo = sexo;
-          this.nacionalidad = nacionalidad;
-          this.lugarResidencia = lugarResidencia;
-          this.estadoCivil = estadoCivil;
-          this.codigoAfiliado = codigoAfiliado;
-          this.origenAfiliado = origenAfiliado;
-          this.situacionAfiliado = situacionAfiliado;
-          this.tipoComision = tipoComision;
-          this.tipoTrabajador = tipoTrabajador;
-          this.afpActual = afpActual;
-          this.fechaIngresoSpp = fechaIngresoSpp;
-          this.fechaDefuncion = fechaDefuncion;
-    }
+    constructor(documento, apellido_paterno, apellido_materno, primer_nombre, segundo_nombre, sexo, nacionalidad, lugar_residencia, estado_civil, cuss, origen_afiliado, situacion, tipo_trabajador, afp_actual, tipo_comision, fecha_defuncion, fecha_afiliacion){
+        this.documento = documento;
+        this.apellido_paterno = apellido_paterno;
+        this.apellido_materno = apellido_materno;
+        this.primer_nombre = primer_nombre;
+        this.segundo_nombre = segundo_nombre;
+        this.sexo = sexo;
+        this.nacionalidad = nacionalidad;
+        this.lugar_residencia = lugar_residencia;
+        this.estado_civil = estado_civil;
+        this.cuss = cuss;
+        this.origen_afiliado = origen_afiliado;
+        this.situacion = situacion;
+        this.tipo_comision = tipo_comision;
+        this.tipo_trabajador = tipo_trabajador;
+        this.afp_actual = afp_actual;
+        this.fecha_afiliacion = fecha_afiliacion;
+        this.fecha_defuncion = fecha_defuncion;
+  }
+
 }
 
 module.exports = AfiliadoModel;
